b7a01d9d6762f13ab8ea60c2e35a6c79d3791a1c
Web Master <webmaster@bprdn.com>
2023-07-17 02:50:31 +0000

    Filter laporan

--- resources/views/admin/laporan/laporan_pdf.blade.php
+++ resources/views/admin/laporan/laporan_pdf.blade.php
@@ -1,44 +1,39 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <title>Sistem Inventaris</title>
-    <meta content='width=device-width, initial-scale=1.0, shrink-to-fit=no' name='viewport' />
-    <link rel="icon" href="/assets/img/icon.ico" type="image/x-icon" />
-
-    <!-- Fonts and icons -->
-    <!-- <script src="/assets/js/plugin/webfont/webfont.min.js"></script> -->
-    <!-- <script>
-    WebFont.load({
-        google: {
-            "families": ["Open+Sans:300,400,600,700"]
-        },
-        custom: {
-            "families": ["Flaticon", "Font Awesome 5 Solid", "Font Awesome 5 Regular", "Font Awesome 5 Brands"],
-            urls: ['/fontawesome/css/all.css']
-        },
-        active: function() {
-            sessionStorage.fonts = true;
-        }
-    });
-    </script> -->
-
-    <!-- CSS Files -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" media="print"
-        crossorigin="anonymous">
-
+    <meta charset="utf-8">
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1"
+    >
+    <title>Bootstrap demo</title>
     <style>
-    body {
-        -webkit-print-color-adjust: exact !important;
-    }
-    </style>
+        * {
+            font-family: system-ui, -apple-system, "Segoe UI", Roboto, "Helvetica Neue", "Noto Sans", "Liberation Sans", Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol", "Noto Color Emoji";
+        }
 
+        table {
+            width: 100%;
+            border-collapse: collapse;
+        }
+
+        table thead {
+            background: black;
+            color: white;
+        }
+
+        table tr {
+            border: 1px solid black;
+        }
+
+        table tr>* {
+            padding: 10px;
+        }
+    </style>
 </head>
 
 <body>
-
     <table class="table">
         <thead class="table-dark">
             <tr>
@@ -55,41 +50,19 @@
         </thead>
         <tbody>
             @php $no=1; @endphp
-            @foreach($barang as $row)
-
-            <tr>
-                <th scope="row">{{ $no++ }}</th>
-                <td>{{ $row->nama_user }}</td>
-                <td>{{ $row->nama_kategori }}</td>
-                <td>{{ $row->nama_lokasi }}</td>
-                <td>{{ $row->nomor_inventaris }}</td>
-                <td>Rp. {{ number_format($row->harga) }}</td>
-                <td>{{ date('d F Y', strtotime($row->tanggal_beli)) }}</td>
-                @endforeach
+            @foreach ($barang as $row)
+                <tr>
+                    <th scope="row">{{ $no++ }}</th>
+                    <td>{{ $row->nama_user }}</td>
+                    <td>{{ $row->nama_kategori }}</td>
+                    <td>{{ $row->nama_lokasi }}</td>
+                    <td>{{ $row->nomor_inventaris }}</td>
+                    <td>Rp. {{ number_format($row->harga) }}</td>
+                    <td>{{ date('d F Y', strtotime($row->tanggal_beli)) }}</td>
+                </tr>
+            @endforeach
         </tbody>
     </table>
-
-
-    <!-- <script src="/assets/js/core/jquery.3.2.1.min.js"></script> -->
-    <!-- <script src="/assets/js/core/popper.min.js"></script> -->
-    <!-- <script src="/assets/js/core/bootstrap.min.js"></script> -->
-
-    <!-- <script src="https://code.jquery.com/jquery-3.6.4.min.js"
-        integrity="sha256-oP6HI9z1XaZNBrJURtCoUT5SUnxFr8s3BzRl+cbzUq8=" crossorigin="anonymous"></script> -->
-    <!-- jQuery UI -->
-    <!-- <script src="/assets/js/plugin/jquery-ui-1.12.1.custom/jquery-ui.min.js"></script> -->
-    <!-- <script src="/assets/js/plugin/jquery-ui-touch-punch/jquery.ui.touch-punch.min.js"></script> -->
-    <!-- Bootstrap Toggle -->
-    <!-- <script src="/assets/js/plugin/bootstrap-toggle/bootstrap-toggle.min.js"></script> -->
-    <!-- jQuery Scrollbar -->
-    <!-- <script src="/assets/js/plugin/jquery-scrollbar/jquery.scrollbar.min.js"></script> -->
-    <!-- Datatables -->
-    <!-- <script src="/assets/js/plugin/datatables/datatables.min.js"></script> -->
-    <!-- Azzara JS -->
-    <!-- <script src="/assets/js/ready.min.js"></script> -->
-    <!-- Azzara DEMO methods, don't include it in your project! -->
-    <!-- <script src="/assets/js/setting-demo.js"></script> -->
-
 </body>
 
 </html>
